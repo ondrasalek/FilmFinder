db20fa79bd8df0680ae631a6dbfd5cef1565e4a2
Ondrej Salek <ondrasalek@gmail.com>
2021-12-10 17:03:34 +0100

    icons and splashs

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,7 +4,23 @@ const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'ionic-app-base',
   webDir: 'www',
-  bundledWebRuntime: false
+  bundledWebRuntime: false,
+  "plugins": {
+    "SplashScreen": {
+      "launchShowDuration": 3000,
+      "launchAutoHide": true,
+      "backgroundColor": "#ffffffff",
+      "androidSplashResourceName": "splash",
+      "androidScaleType": "CENTER_CROP",
+      "androidSpinnerStyle": "large",
+      "iosSpinnerStyle": "small",
+      "spinnerColor": "#999999",
+      "showSpinner": true,
+      "splashFullScreen": true,
+      "splashImmersive": true
+    }
+  }
 };
+
 
 export default config;
