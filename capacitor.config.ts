0c9d249eb387a3b421375ce5526e84f95ced4313
Ondrej Salek <57462486+ondrasalek@users.noreply.github.com>
2021-12-13 10:29:03 +0100

    edit name

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'Movie Finder',
+  appName: 'Film Finder',
   webDir: 'www',
   bundledWebRuntime: false,
 };
